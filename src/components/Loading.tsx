3c2f274429aebdd0dc4c28876908b78dcf50eea9
sharduldabhane <shardul.dabhane@gmail.com>
2024-01-09 17:30:22 -0500

    Sixteenth commit: Loading CSS added

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "./Loading.module.css"; // Import the CSS module
 
-const Loading: React.FC = () => <div>Loading...</div>;
+const Loading: React.FC = () => (
+  <div className={styles.loadingContainer}>
+    <div className={styles.loadingSpinner}></div>
+  </div>
+);
 
 export default Loading;
